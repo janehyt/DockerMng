f920ed217db6186ac904a6c3dc397fe6aa56afa8
janehyt <jane_hyt@163.com>
2016-04-17 14:47:41 +0800

    Delete _pycache_ and add to ignore list; change api auth cofig

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'PAGE_SIZE': 10
 }
 
